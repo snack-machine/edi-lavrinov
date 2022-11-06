887da48d5930542c031f6cdd38c4bae443a975ec
Ilya Lavrinov <illia.lavrinov@sigma.software>
2022-11-06 21:27:30 +0200

    Rename project folder

---
+++ chat/common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+#define DEFAULT_PORT 32001
+#define DEFAULT_IP "127.0.0.1"
+#define BUFFER_SIZE 256
+
+#endif
